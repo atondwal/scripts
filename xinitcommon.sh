9011660950d0b9128857d6d3e74ab6eff21d17a0
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-10-29 10:39:07 -0700

    Added three scripts

---
+++ xinitcommon.sh
@@ -0,0 +1,7 @@
+# xinitcomom
+
+xflux -z 98115
+sh ~/.fehbg
+xsetroot -cursor_name left_ptr
+xcompmgr -c &
+exec xmonad
---
+++ xmonad.hs
@@ -0,0 +1,19 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig
+import System.IO
+
+main = xmonad $ defaultConfig
+     	    { borderWidth = 2
+	    , terminal 	= "urxvt"
+	    , normalBorderColor = "#cccccc"
+	    , focusedBorderColor = "#112255"
+	    , focusFollowsMouse = False
+	    , manageHook = manageDocks <+> manageHook defaultConfig
+	    , layoutHook = avoidStruts $ layoutHook defaultConfig
+	    , modMask = mod4Mask}
+	    `additionalKeysP`
+	    [ ("M-e", spawn "emacs")
+	    , ("M-f", spawn "conkeror")]	
