93bf3bb95485cc1cd48facecf7df36e6394eacd3
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-11-13 13:20:10 -0800

    Changed Some Config Files

--- xinitcommon.sh
+++ xinitcommon.sh
@@ -4,4 +4,5 @@ xflux -z 98115
 sh ~/.fehbg
 xsetroot -cursor_name left_ptr
 xcompmgr -c &
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 6 --transparent true --alpha 0 --tint 0x000000 --height 16 &
 exec xmonad
--- xmonad.hs
+++ xmonad.hs
@@ -5,15 +5,22 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
 import System.IO
 
-main = xmonad $ defaultConfig
-     	    { borderWidth = 2
-	    , terminal 	= "urxvt"
-	    , normalBorderColor = "#cccccc"
-	    , focusedBorderColor = "#112255"
-	    , focusFollowsMouse = False
-	    , manageHook = manageDocks <+> manageHook defaultConfig
-	    , layoutHook = avoidStruts $ layoutHook defaultConfig
-	    , modMask = mod4Mask}
-	    `additionalKeysP`
-	    [ ("M-e", spawn "emacs")
-	    , ("M-f", spawn "firefox")]	
+main = do
+     xmproc <- spawnPipe "/usr/bin/xmobar"
+     xmonad $ defaultConfig
+     	    	    { borderWidth = 2
+	    	    , terminal 	= "urxvt"
+		    , normalBorderColor = "#cccccc"
+		    , focusedBorderColor = "#112255"
+		    , focusFollowsMouse = False
+		    , manageHook = manageDocks <+> manageHook defaultConfig
+		    , layoutHook = avoidStruts $ layoutHook defaultConfig
+		    , logHook = dynamicLogWithPP xmobarPP
+		      { ppOutput = hPutStrLn xmproc
+		      ,	ppTitle = xmobarColor "blue" "" . shorten 50
+		      , ppLayout = const "" -- to disable the layout info on xmobar
+		      }
+		    , modMask = mod4Mask}
+		    `additionalKeysP`
+		    [ ("M-e", spawn "emacs")
+		    , ("M-f", spawn "firefox")]
