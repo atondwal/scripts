7a8da2e5e3056ad9a25bc62e98a6175c63944ae8
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2015-02-11 22:58:16 -0800

    Added caps-control

--- xinitcommon.sh
+++ xinitcommon.sh
@@ -5,4 +5,5 @@ sh ~/.fehbg
 xsetroot -cursor_name left_ptr
 xcompmgr -c &
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 6 --transparent true --alpha 0 --tint 0x000000 --height 16 &
+setxkbmap -option ctrl:nocaps
 exec xmonad
