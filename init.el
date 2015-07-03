1c1aa8ffa5d78efeb101dfe0aadde3ae3e7e542a
Alex Sanchez-Stern <alex@alexMobile.alex.mobile.local>
2015-07-03 04:08:14 -0700

    Added init.el with usepackage

---
+++ init.el
@@ -0,0 +1,59 @@
+;; Set up package manager
+(add-to-list 'load-path "~/.emacs.d/lisp/use-package/")
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
+
+(use-package package
+  :config
+  (add-to-list 'package-archives
+	       '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives
+	       '("marmalade" . "http://marmalade-repo.org/packages/") t)
+  (package-initialize))
+
+;; Theme stuff
+(use-package zenburn-theme
+	     :config
+	     (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+	     (load-theme 'zenburn t)
+	     :ensure t)
+
+(use-package geiser :ensure t)
+(use-package quack :ensure t)
+
+(use-package magit
+  :bind ("C-x m" . magit-status)
+  :ensure t)
+(use-package tramp :ensure t)
+(use-package ido
+  :config
+  (setq ido-enable-flex-matching t)
+  (setq ido-everywhere t)
+  (ido-mode t)
+  :ensure t)
+
+;; (use-package alloy-mode
+;;   :mode "\\.als$"
+;;   :ensure t)
+;; (use-package dafny-mode
+;;   :mode "\\.dfy$"
+;;   :ensure t)
+
+(use-package wgrep :ensure t)
+;; Lisp Stuff
+(define-abbrev-table 'scheme-mode-abbrev-table
+  '(("lambda" "λ" nil 1)))
+(setq default-abbrev-mode t)
+
+;; Preferences
+(require 'dired-x)
+(setf inhibit-startup-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(setf initial-buffer-choice default-directory)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+(setq-default dired-omit-files-p t)
+(setq dired-omit-files (concat dired-omit-files "\\|^\\..+$"))
