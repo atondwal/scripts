c030823a3619f514f3214be1654f831e9a02bd62
Alex Sanchez-Stern <alex@alexMobile.alex.mobile.local>
2015-07-18 00:24:23 -0700

    Added a racket script to install the scripts

---
+++ installScripts.rkt
@@ -0,0 +1,26 @@
+#!/bin/racket
+#lang racket
+
+(when (file-exists? "~/.emacs")
+  (rename-file-or-directory "~/.emacs ~/.emacs.d/.emacs~"))
+(define (softlink-file from to)
+  (system (format "ln -s ~a ~a" from to)))
+(define (eh path-string)
+  (string-replace path-string "~/"
+                  (path->string (find-system-path 'home-dir))))
+(map (λ (pair)
+       (match-let* ([`(,from ,to) pair]
+                   [to (eh to)])
+         (when (file-exists? to)
+           (rename-file-or-directory to (format "~a~~" to) #t))
+         (softlink-file from to)))
+     '(("init.el" "~/.emacs.d/init.el")
+       ("dotXdefaults" "~/.Xdefaults")
+       ("dotxmobarrc" "~/.xmobarrc")
+       ("sshconfig" "~/.ssh/config")
+       ("xmonad.hs" "~/.xmonad/xmonad.hs")))
+
+(display-to-file (format "sh ~axinitcommon.sh" (current-directory))
+                 (eh "~/.profile")
+                 #:mode 'text
+                 #:exists 'append)
