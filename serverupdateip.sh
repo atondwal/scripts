da8eb082d5571d7dcb307461ff1a592be0461ba9
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-10-02 13:37:29 -0700

    Clean up unused scripts

--- serverupdateip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sshpass -p admin ssh router /sbin/ifconfig | grep inet | tail -n 1 | cut -d":" -f2 | cut -d" " -f1 | ssh uwplse.org "cat > /var/www/alex/homeserver_ip.txt"
--- updateserverip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-NEWIP=$(wget -O - -o /dev/null alex.uwplse.org/homeserver_ip.txt)
-NEWLINE="$NEWIP alex.server.remote serverRemote"
-sudo sed -i "8s/.*/$NEWLINE/" /etc/hosts
