2187bd32db2d00708deb1274920a1cb8c02a185e
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2017-04-29 14:19:30 -0700

    Even more xmonad stuff

--- xmonad.hs
+++ xmonad.hs
@@ -7,14 +7,18 @@ import XMonad.Layout.PerWorkspace
 import XMonad.Layout.SimplestFloat
 import XMonad.Layout.NoBorders
 import System.IO
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.Fullscreen
 
 myWorkspaces = ["edit", "web", "term", "read", "chat"] ++ map show [6..9]
 myManageHook = composeAll . concat $
                [ [ className =? c --> doFloat | c <- cFloats ]
                , [ title =? t --> doFloat | t <- tFloats ]
+               , [ isFullscreen --> doFullFloat ]
                , [ className =? "Evince" -->doShift "read"]
                , [ className =? "HipChat" -->doShift "chat"]
-               , [ className =? "Slack" -->doShift "chat"]]
+               , [ className =? "ScudCloud" -->doShift "chat"]
+               , [ className =? "Firefox" -->doShift "web"]]
   where cFloats = ["NES", "tilda"]
         tFloats = ["Firefox Preferences", "Downloads", "Add-ons", "Rename", "Create", "tilda" ]
 
@@ -24,18 +28,24 @@ main = do
                     { borderWidth = 2
                     , terminal  = "emacsclient -c -e '(eshell)'"
                     , workspaces = myWorkspaces
-		    , normalBorderColor = "#cccccc"
-		    , focusedBorderColor = "#112255"
-		    , focusFollowsMouse = False
-		    , manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
-		    , layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
-		    , logHook = dynamicLogWithPP xmobarPP
-		      { ppOutput = hPutStrLn xmproc
-		      ,	ppTitle = xmobarColor "green" "" . shorten 50
-		      , ppLayout = const "" -- to disable the layout info on xmobar
-		      }
-		    , modMask = mod4Mask}
-		    `additionalKeysP`
-		    [ ("M-e", spawn "emacsclient -c")
-		    , ("M-f", spawn "firefox")
-                    , ("M-s", sendMessage ToggleStruts)]
+                    , normalBorderColor = "#cccccc"
+                    , focusedBorderColor = "#112255"
+                    , focusFollowsMouse = False
+                    , manageHook = myManageHook <+> manageDocks <+> manageHook def
+                    , layoutHook = avoidStruts $ smartBorders $ layoutHook def
+                    , handleEventHook = docksEventHook <+> fullscreenEventHook <+> handleEventHook def
+                    , logHook = dynamicLogWithPP xmobarPP
+                      { ppOutput = hPutStrLn xmproc
+                      , ppTitle = xmobarColor "green" "" . shorten 50
+                      , ppLayout = const "" -- to disable the layout info on xmobar
+                      }
+                    , modMask = mod4Mask}
+                    `additionalKeysP`
+                    [ ("M-e", spawn "emacsclient -c")
+                    , ("M-f", spawn "tor-browser")
+                    , ("M-c", spawn "hipchat")
+                    , ("M-d", spawn "update-monitor")
+                    , ("M-b", spawn "next-background")
+                    , ("M-s", sendMessage ToggleStruts)
+                    , ("XF86AudioRaiseVolume", spawn "amixer -q sset Master 10%+")
+                    , ("XF86AudioLowerVolume", spawn "amixer -q sset Master 10%-")]
